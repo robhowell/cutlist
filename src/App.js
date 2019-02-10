1a9ea84c50f827f9b4d84db192153b4cfc6d20a0
Rob Howell <rob@robhowell.com>
2019-02-10 19:51:19 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const StyledApp = styled.main`
+  padding: 10px;
+`;
+
+const StyledRect = styled.rect`
+  fill: rgb(200,200,200);
+`;
 
 class App extends Component {
+  state = {
+    requiredPieces: []
+  }
+
   render() {
+    const { requiredPieces } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <StyledApp>
+        {
+          requiredPieces.map(requiresPiece => (
+            <svg width="202" height="202">
+              <StyledRect width="200" height="200" />
+            </svg>
+          ))
+        }
+
+        {
+          requiredPieces.map(requiresPiece => (
+            <svg width="202" height="202">
+              <StyledRect width="200" height="200" />
+            </svg>
+          ))
+        }
+      </StyledApp>
     );
   }
 }
